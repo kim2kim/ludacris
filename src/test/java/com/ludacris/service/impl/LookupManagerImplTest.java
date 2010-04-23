abf053cdddf52e13c63082ee918969b58d34441a
kim2kim <kim2kim@944f3d36-7708-ea8d-c7f3-27e5a972e4fd>
2010-04-23 02:58:41 +0000

    vanila sandbox

---
+++ src/test/java/com/ludacris/service/impl/LookupManagerImplTest.java
@@ -0,0 +1,42 @@
+package com.ludacris.service.impl;
+
+import com.ludacris.dao.LookupDao;
+import com.ludacris.model.Role;
+import com.ludacris.model.LabelValue;
+import com.ludacris.Constants;
+import org.jmock.Expectations;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class LookupManagerImplTest extends BaseManagerMockTestCase {
+    private LookupManagerImpl mgr = new LookupManagerImpl();
+    private LookupDao lookupDao;
+
+    @Before
+    public void setUp() throws Exception {
+        lookupDao = context.mock(LookupDao.class);
+        mgr.dao = lookupDao;
+    }
+
+    @Test
+    public void testGetAllRoles() {
+        log.debug("entered 'testGetAllRoles' method");
+
+        // set expected behavior on dao
+        Role role = new Role(Constants.ADMIN_ROLE);
+        final List<Role> testData = new ArrayList<Role>();
+        testData.add(role);
+        context.checking(new Expectations() {{
+            one(lookupDao).getRoles();
+            will(returnValue(testData));
+        }});
+
+        List<LabelValue> roles = mgr.getAllRoles();
+        assertTrue(roles.size() > 0);
+    }
+}
---
+++ src/test/java/com/ludacris/webapp/controller/UserControllerTest.java
@@ -0,0 +1,17 @@
+package com.ludacris.webapp.controller;
+
+import java.util.Map;
+
+import com.ludacris.Constants;
+import org.springframework.web.servlet.ModelAndView;
+
+public class UserControllerTest extends BaseControllerTestCase {
+
+    public void testHandleRequest() throws Exception {
+        UserController c = (UserController) applicationContext.getBean("userController");
+        ModelAndView mav = c.handleRequest(null, null);
+        Map m = mav.getModel();
+        assertNotNull(m.get(Constants.USER_LIST));
+        assertEquals("admin/userList", mav.getViewName());
+    }
+}
